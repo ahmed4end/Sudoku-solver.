5d92c3e4ad59814a68d9fa72597b2599294e5409
ahmed4end <35382418+ahmed4end@users.noreply.github.com>
2019-12-21 19:52:37 +0200

    Update Sudoku.py
    added some comments on code so it be better to understand.

--- Sudoku.py
+++ Sudoku.py
@@ -1,14 +1,14 @@
 def solve(sudoku): #using recursion and backtracking, here we go.
-	empties = [(i,j) for i in range(9) for j in range(9) if sudoku[i][j] == 0]
-	predict = lambda i, j: set(range(1,10))-set([sudoku[y+range(1,10,3)[i//3]][x+range(1,10,3)[j//3]] for y in (-1,0,1) for x in (-1,0,1)])-set(sudoku[i])-set(list(zip(*sudoku))[j])
-	if len(empties)==0:return True
-	gap = next(iter(empties))
+	empties = [(i,j) for i in range(9) for j in range(9) if sudoku[i][j] == 0]                       #this step detects the empty cells to fill it later.
+	predict = lambda i, j: set(range(1,10))-set([sudoku[y+range(1,10,3)[i//3]][x+range(1,10,3)[j//3]] for y in (-1,0,1) for x in (-1,0,1)])-set(sudoku[i])-set(list(zip(*sudoku))[j])                       #this lambda function predicts all valid values for a certain cell.
+	if len(empties)==0:return True                                                                   #recursion's endline trigger to avoid infinite loops (triggered when all cells are filled.).
+	gap = next(iter(empties))                                                                        
 	predictions = predict(*gap)
 	for i in predictions:
 		sudoku[gap[0]][gap[1]] = i
 		if solve(sudoku):return True
 		sudoku[gap[0]][gap[1]] = 0
-	return False
+	return False                                                                                     
 
 if __name__ == "__main__":
 	sudoku = [[0,0,0,6,0,1,0,0,0], 
